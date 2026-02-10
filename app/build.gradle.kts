a48118a245080edceb03a947b4096718382fe7b4
educacaomggovbr00-web <educacaomggovbr00@gmail.com>
2026-02-10 17:35:27 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("com.android.application")
     kotlin("android")
+    // O PLUGIN QUE ESTAVA FALTANDO PARA O KOTLIN 2.0
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" 
 }
 
 val APP_VERSION_NAME : String by project
@@ -27,9 +29,7 @@ android {
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
+    // Com o plugin novo, não precisamos mais da linha 'kotlinCompilerExtensionVersion' aqui dentro!
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -50,17 +50,15 @@ tasks.withType<KotlinCompile>().configureEach {
     }
 }
 
-// Isso aqui mata o fiscal chato (Detekt)
 tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
     ignoreFailures = true
 }
 
 dependencies {
-    // IMPORTAÇÕES DIRETAS (Sem usar o libs. do catálogo que deu erro)
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.01.00")) // Versão atualizada
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")

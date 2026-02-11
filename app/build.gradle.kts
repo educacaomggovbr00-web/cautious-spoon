dd134316d5360c55f022e2f88478ffc9c1a1af9e
educacaomggovbr00-web <educacaomggovbr00@gmail.com>
2026-02-11 19:50:28 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -9,7 +9,6 @@ plugins {
 
 android {
     compileSdk = 34
-    // O namespace aqui substitui o package do Manifesto e resolve o erro de 1m 20s
     namespace = "com.ncorti.kotlin.template.app"
 
     defaultConfig {
@@ -25,7 +24,6 @@ android {
         compose = true
     }
 
-    // Blindagem para ignorar erros de análise de código e focar no APK
     lint {
         abortOnError = false
         checkReleaseBuilds = false
@@ -51,20 +49,24 @@ tasks.withType<KotlinCompile>().configureEach {
     }
 }
 
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-    ignoreFailures = true
-}
-
 dependencies {
+    // BASE DO SISTEMA
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+    
+    // COMPOSE MATERIAL & ICONS (Resolve o erro dos ícones)
     implementation(platform("androidx.compose:compose-bom:2024.01.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
-    
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    // MOTOR MONSTRO V19 (As peças que faltavam no log)
     implementation("androidx.media3:media3-exoplayer:1.2.0")
     implementation("androidx.media3:media3-ui:1.2.0")
     implementation("androidx.media3:media3-common:1.2.0")
+    implementation("androidx.media3:media3-transformer:1.2.0") // O renderizador real
+    implementation("androidx.media3:media3-effect:1.2.0")      // Os efeitos GL
 }
